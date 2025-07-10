069e800744553863be04093b6e5e4af05a245951
Kelvin_chovatiya <kelvinchovatiya17@gmail.com>
2025-07-10 15:14:25 +0530

    Update P1_2.cs

--- P1_2.cs
+++ P1_2.cs
@@ -10,14 +10,14 @@ namespace CE_BTECH
     {
         public static void Main()
         {
-            Console.WriteLine("Name: Nakum Ankit");
-            Console.WriteLine("DOB: 03-02-2006");
-            Console.WriteLine("Address: Karnavati International School,Railnagar");
-            Console.WriteLine("City: Rajkot");
-            Console.WriteLine("Pincode: 360001");
+            Console.WriteLine("Name:Kelvin Chovatiya");
+            Console.WriteLine("DOB: 17-12-2005");
+            Console.WriteLine("Address: mohanpark");
+            Console.WriteLine("City: Gondal");
+            Console.WriteLine("Pincode: 360311");
             Console.WriteLine("State: Gujarat");
             Console.WriteLine("Country: India");
-            Console.WriteLine("Email: anakum729@rku.ac.in");
+            Console.WriteLine("Email: kchovatiya336@rku.ac.in");
         }
     }
 }
