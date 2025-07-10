c4c58aacda5ed053d3b4d25f54270ddcfac303c8
Kelvin_chovatiya <kelvinchovatiya17@gmail.com>
2025-07-10 14:58:20 +0530

    Add project files.

---
+++ P1_6.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CE_BTECH
+{
+        class P1_6
+        {
+            static void Main(string[] args)
+            {
+                int a, b, c, result;
+                string str;
+
+                Console.Write("Enter Number 1:");
+                str = Console.ReadLine();
+                a = Convert.ToInt32(str);
+
+                Console.Write("Enter Number 2:");
+                str = Console.ReadLine();
+                b = Convert.ToInt32(str);
+
+                Console.Write("Enter Number 3:");
+                str = Console.ReadLine();
+                c = Convert.ToInt32(str);
+
+                result = Sum(a, b, c);
+                Console.WriteLine("Sum:" + result);
+
+                Console.Read();
+            }
+
+            static int Sum(int x, int y, int z)
+            {
+                int res;
+                res = x + y + z;
+                return res;
+            }
+        }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tutorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+           for (int i=1;i<=9;i++)
+            {
+                int k=1+i;
+                for (int j = 1; j <=k; j++)
+                {
+                    Console.Write("*");
+                    if (i <= 5)
+                    {
+                        k =i;
+                    }
+                    else
+                    {
+                        k = 10 - i;
+                    }
+
+
+                }
+                
+                Console.WriteLine();
+                
+            }
+          
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
